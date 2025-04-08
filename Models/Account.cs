16e29f745a9b3f75e19c3209626d235ca7ae9ff4
gajpawel <pg305038@student.polsl.pl>
2025-04-08 20:18:15 +0200

    Dodaj pliki projektów.

---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightAlright.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+    }
+}
