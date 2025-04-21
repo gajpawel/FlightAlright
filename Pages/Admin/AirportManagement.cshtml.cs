9db8d6bc3596e1dab764c399a74482c3e2a0bf1b
BamB00s <kombinator1000@gmail.com>
2025-04-21 22:36:24 +0200

    janky menu for admin action navigation(do zmiany)

---
+++ Pages/Admin/AirportManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlightAlright.Pages.Admin
+{
+    public class AirportManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Admin/PersonelManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlightAlright.Pages.Admin
+{
+    public class PersonelManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
