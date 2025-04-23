258a8791f1423430594fa2ecd2e4f0a1a46a2049
dawid <dawid.szkorupa@gmail.com>
2025-04-23 09:48:45 +0200

    Dodawanie lotnisk

--- Pages/Admin/AirportManagement.cshtml.cs
+++ Pages/Admin/AirportManagement.cshtml.cs
@@ -1,3 +1,5 @@
+using FlightAlright.Data;
+using FlightAlright.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace FlightAlright.Pages.Admin
 {
     public class AirportManagementModel : PageModel
     {
+        private readonly FlightAlrightContext _context;
+
+        public AirportManagementModel(FlightAlrightContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Airport NewAirport { get; set; } = new();
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Airport.Add(NewAirport);
+            await _context.SaveChangesAsync();
+
+           
+            return RedirectToPage(); // Odœwie¿enie po dodaniu
         }
     }
 }
