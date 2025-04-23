2f3eaed0cb2b22b0234a4deacd01d5ecf3f9c816
ItsMeRaijiN <igorpotoczny35@gmail.com>
2025-04-23 16:28:26 +0200

    wstępne dodawanie kont pracowniczych

--- Pages/Employees/Create.cshtml.cs
+++ Pages/Employees/Create.cshtml.cs
@@ -1,46 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using FlightAlright.Data;
 using FlightAlright.Models;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FlightAlright.Pages.Employees
 {
     public class CreateModel : PageModel
     {
-        private readonly FlightAlright.Data.FlightAlrightContext _context;
+        private readonly FlightAlrightContext _context;
 
-        public CreateModel(FlightAlright.Data.FlightAlrightContext context)
+        public CreateModel(FlightAlrightContext context)
         {
             _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-        ViewData["AccountId"] = new SelectList(_context.Set<Account>(), "Id", "Id");
-        ViewData["PositionId"] = new SelectList(_context.Set<Position>(), "Id", "Id");
-            return Page();
         }
 
         [BindProperty]
         public Employee Employee { get; set; } = default!;
 
-        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public void OnGet()
+        {
+            var usedAccountIds = _context.Employee.Select(e => e.AccountId).ToList();
+            var availableAccounts = _context.Account
+                .Where(a => !usedAccountIds.Contains(a.Id))
+                .ToList();
+
+            ViewData["Accounts"] = new SelectList(availableAccounts, "Id", "Login");
+            ViewData["Positions"] = new SelectList(_context.Position, "Id", "Name");
+        }
+
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
-            {
                 return Page();
-            }
 
             _context.Employee.Add(Employee);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("/Admin/PersonelManagement");
         }
     }
 }
