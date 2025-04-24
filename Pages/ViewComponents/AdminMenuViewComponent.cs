63ee211ea4436dfe4f22fec1a583117ef25aa1eb
BamB00s <kombinator1000@gmail.com>
2025-04-24 10:26:53 +0200

    dodano rozszerzalne menu admina

---
+++ Pages/ViewComponents/AdminMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightAlright.Pages.viewComponents
+{
+    public class AdminMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var currentPage = ViewContext.RouteData.Values["Page"]?.ToString() ?? "";
+
+            var menuItems = new List<(string Title, string PageName)>
+        {
+            ("Konto", "./AdminProfile"),
+            ("Personel", "./PersonelManagement"),
+            ("Lotniska", "./AirportManagement"),
+            ("Flota", "./FleetManagement"),
+            //("Loty", "./FlightManagement") // psuje sie przy wyborze
+        };
+
+            return View((currentPage, menuItems));
+        }
+    }
+
+}
